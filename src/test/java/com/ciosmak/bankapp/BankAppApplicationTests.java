daa5e3e37d1d08ec4fc242460b71589f11b8c749
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-04 09:56:40 +0100

    Initial commit

---
+++ src/test/java/com/ciosmak/bankapp/BankAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ciosmak.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
