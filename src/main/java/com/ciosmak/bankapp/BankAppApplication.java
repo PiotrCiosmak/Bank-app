9c3c351b126b03d1fd858f17b4dfd90edbdb79eb
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-07 23:56:40 +0100

    mainMenu added

--- src/main/java/com/ciosmak/bankapp/BankAppApplication.java
+++ src/main/java/com/ciosmak/bankapp/BankAppApplication.java
@@ -1,15 +1,36 @@
 package com.ciosmak.bankapp;
 
+import com.ciosmak.bankapp.menu.Menu;
+import com.ciosmak.bankapp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
-public class BankAppApplication
+public class BankAppApplication implements CommandLineRunner
 {
 
     public static void main(String[] args)
     {
         SpringApplication.run(BankAppApplication.class, args);
     }
+
+    @Override
+    public void run(String[] args)
+    {
+        String loginOption = Menu.mainLoginMenu();
+        switch (loginOption)
+        {
+            case "sign_in" -> userService.signIn();
+            case "register" -> userService.register();
+        }
+
+    }
+
+    private final UserService userService;
 
 }
