07204b6f701d0f39cb9bdb8aa63d78266d7d3df3
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-07 23:55:48 +0100

    finding by personal identity number added

--- src/main/java/com/ciosmak/bankapp/repository/PersonalDataRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/PersonalDataRepository.java
@@ -3,6 +3,9 @@ package com.ciosmak.bankapp.repository;
 import com.ciosmak.bankapp.entity.PersonalData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonalDataRepository extends JpaRepository<PersonalData, Long>
 {
+    Optional<PersonalData> findByPersonalIdentityNumber(String identityNumber);
 }
