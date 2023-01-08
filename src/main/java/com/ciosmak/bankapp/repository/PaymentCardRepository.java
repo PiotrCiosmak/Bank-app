3a201cf011a19d0d55861faea7895f9c55305a21
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-08 21:17:16 +0100

    finding by card number added

--- src/main/java/com/ciosmak/bankapp/repository/PaymentCardRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/PaymentCardRepository.java
@@ -3,7 +3,9 @@ package com.ciosmak.bankapp.repository;
 import com.ciosmak.bankapp.entity.PaymentCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long>
 {
-    PaymentCard findByCardNumber(String cardNumber);
+    Optional<PaymentCard> findByCardNumber(String cardNumber);
 }
