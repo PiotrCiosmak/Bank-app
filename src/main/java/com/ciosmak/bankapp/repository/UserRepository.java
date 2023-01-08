580896fcd033821351415941ed47feb7bfedbbfc
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-08 02:16:21 +0100

    finding user by id

--- src/main/java/com/ciosmak/bankapp/repository/UserRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/UserRepository.java
@@ -7,5 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>
 {
+    @Override
+    Optional<User> findById(Long id);
+
     Optional<User> findByEmail(String email);
 }
