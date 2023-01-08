32f539f3bc2ffc294722e31aa5255b3a66fb7223
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-08 21:19:54 +0100

    finding by bank account number added

--- src/main/java/com/ciosmak/bankapp/repository/BankAccountRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/BankAccountRepository.java
@@ -3,7 +3,9 @@ package com.ciosmak.bankapp.repository;
 import com.ciosmak.bankapp.entity.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long>
 {
-    BankAccount findByBankAccountNumber(String bankAccountNumber);
+    Optional<BankAccount> findByBankAccountNumber(String bankAccountNumber);
 }
