5088d91137a7dad2be4fa1f51e9684c2edcd1ad6
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-09 15:12:22 +0100

    finding by Userid removed

--- src/main/java/com/ciosmak/bankapp/repository/PaymentCardRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/PaymentCardRepository.java
@@ -3,12 +3,9 @@ package com.ciosmak.bankapp.repository;
 import com.ciosmak.bankapp.entity.PaymentCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long>
 {
     Optional<PaymentCard> findByCardNumber(String cardNumber);
-
-    ArrayList<PaymentCard> findByUserId(Long userId);
 }
