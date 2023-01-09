91f373d5caa3c813f9d3de42c0bf20c890eb0f66
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-09 01:57:37 +0100

    finding by user id added

--- src/main/java/com/ciosmak/bankapp/repository/BankAccountRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/BankAccountRepository.java
@@ -3,9 +3,12 @@ package com.ciosmak.bankapp.repository;
 import com.ciosmak.bankapp.entity.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long>
 {
     Optional<BankAccount> findByBankAccountNumber(String bankAccountNumber);
+
+    ArrayList<BankAccount> findByUserId(Long userId);
 }
