8d142b6373cddfbf9fc59cba1f20211ea26a4151
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-09 15:14:31 +0100

    singleton for bank account id added

---
+++ src/main/java/com/ciosmak/bankapp/bank/account/id/BankAccountId.java
@@ -0,0 +1,32 @@
+package com.ciosmak.bankapp.bank.account.id;
+
+
+public final class BankAccountId
+{
+    private static BankAccountId instance;
+    public Long id;
+
+    private BankAccountId(Long id)
+    {
+        this.id = id;
+    }
+
+    public static BankAccountId getInstance(Long id)
+    {
+        if (instance == null)
+        {
+            instance = new BankAccountId(id);
+        }
+        return instance;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public Long getId()
+    {
+        return this.id;
+    }
+}
