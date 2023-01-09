5addfb1a2d992050c174c715f1c658e93ade843f
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-09 22:30:57 +0100

    find by id added findAll added

--- src/main/java/com/ciosmak/bankapp/repository/PaymentCardRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/PaymentCardRepository.java
@@ -3,9 +3,15 @@ package com.ciosmak.bankapp.repository;
 import com.ciosmak.bankapp.entity.PaymentCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long>
 {
+    @Override
+    Optional<PaymentCard> findById(Long paymentCardId);
+
+    ArrayList<PaymentCard> findAll();
+
     Optional<PaymentCard> findByCardNumber(String cardNumber);
 }
