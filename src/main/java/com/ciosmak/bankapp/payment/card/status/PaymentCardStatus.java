16975bb62bc98a2752e7757faf56a687e5d02621
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-01-09 22:31:20 +0100

    all function added

--- src/main/java/com/ciosmak/bankapp/payment/card/status/PaymentCardStatus.java
+++ src/main/java/com/ciosmak/bankapp/payment/card/status/PaymentCardStatus.java
@@ -1,6 +1,19 @@
 package com.ciosmak.bankapp.payment.card.status;
 
+import com.ciosmak.bankapp.payment.card.id.PaymentCardId;
+import com.ciosmak.bankapp.repository.PaymentCardRepository;
+
 public interface PaymentCardStatus
 {
-    //TODO wszystkie akcje dla karty płatniczej (zależne od stanu)
+    void showPaymentCard(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void changeLimits(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void blockTemporarily(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void unlock(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void blockPermanently(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void changePin(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void changeContactlessTransactionOption(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void changeMagneticStripOption(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void changeTransactionsWithDdcServiceOption(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void changeSurchargeTransactionsOption(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
+    void changeDebitOption(PaymentCardId paymentCardId, PaymentCardRepository paymentCardRepository);
 }
