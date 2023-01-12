1cbb39dfccb98c107d4d635d1b601d63cd23f407
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-01-12 22:14:56 +0100

    find all added

--- src/main/java/com/ciosmak/bankapp/repository/TransferRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/TransferRepository.java
@@ -3,6 +3,9 @@ package com.ciosmak.bankapp.repository;
 import com.ciosmak.bankapp.entity.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+
 public interface TransferRepository extends JpaRepository<Transfer, Long>
 {
+    ArrayList<Transfer> findAll();
 }
