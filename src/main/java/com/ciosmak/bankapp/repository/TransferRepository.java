b65efecd83104dbc386b2efe633841cd9b4369ac
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-01-13 00:49:09 +0100

    find by sender bank account number added

--- src/main/java/com/ciosmak/bankapp/repository/TransferRepository.java
+++ src/main/java/com/ciosmak/bankapp/repository/TransferRepository.java
@@ -4,8 +4,11 @@ import com.ciosmak.bankapp.entity.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public interface TransferRepository extends JpaRepository<Transfer, Long>
 {
     ArrayList<Transfer> findAll();
+
+    ArrayList<Transfer> findBySenderBankAccountNumber(String senderBankAccountNumber);
 }
