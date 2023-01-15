4a9db54151473b22ecc15b1ca45adc21b36b3261
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-01-15 16:27:18 +0100

    java doc added

--- src/main/java/com/ciosmak/bankapp/bank/account/id/BankAccountId.java
+++ src/main/java/com/ciosmak/bankapp/bank/account/id/BankAccountId.java
@@ -1,15 +1,26 @@
 package com.ciosmak.bankapp.bank.account.id;
 
+/**
+ * This class represents a bank account ID.
+ *
+ * <p> The class uses the Singleton pattern, where only one instance of the class can be created.
+ * This ensures that there is only one unique ID throughout the system. The class can be accessed globally via the
+ * {@link #getInstance(Long)} method.
+ *
+ * <p> The class has methods for setting and getting the ID, and the ID is stored as a private variable.
+ * The constructor is private, so the class can only be instantiated within the class.
+ *
+ * @author  Author Piotr Ciosmak
+ * @version 1.0
+ */
 public final class BankAccountId
 {
-    private static BankAccountId instance;
-    public Long id;
-
-    private BankAccountId(Long id)
-    {
-        this.id = id;
-    }
-
+    /**
+     * Returns the instance of the class
+     *
+     * @param id the id of bank account
+     * @return the instance of the class
+     */
     public static BankAccountId getInstance(Long id)
     {
         if (instance == null)
@@ -19,13 +30,44 @@ public final class BankAccountId
         return instance;
     }
 
+    /**
+     * Set the id of bank account
+     *
+     * @param id the id of bank account
+     */
     public void setId(Long id)
     {
         this.id = id;
     }
 
+    /**
+     * Get the id of bank account
+     *
+     * @return the id of bank account
+     */
     public Long getId()
     {
         return this.id;
     }
+
+
+    /**
+     * The id of bank account
+     */
+    public Long id;
+
+    /**
+     *  Constructor is private, so the class can only be instantiated within the class.
+     *
+     * @param id the id of bank account
+     */
+    private BankAccountId(Long id)
+    {
+        this.id = id;
+    }
+
+    /**
+     * The instance of the class
+     */
+    private static BankAccountId instance;
 }
